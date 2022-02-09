63655a73a17256241aae2ba56356e2ff3d03476b
Wafa Mohessin <wafamohessin@gmail.com>
2022-02-08 23:11:46 -0500

    fetch images from graphql CMS

--- components/Postcard.jsx
+++ components/Postcard.jsx
@@ -17,7 +17,28 @@ const Postcard = ({post}) => {
                 <img 
                 src={post.featuredImage.url} 
                 alt ={post.title} 
-                className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"/>
+                className="max-width: 24rem object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"/>
+            </div>
+            <h1 className= "transition duration-100 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
+                <Link href= {`/post/${post.slug}`}>
+                    {post.title}
+                </Link>
+            </h1>
+            <div classname=" block lg:flex text-center items-center justify-center mb-8 w-full">
+                <div clssName= "flex item-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
+                    <img 
+                    alt={post.author.name}
+                    height="50px"
+                    width="50px"
+                    className= "align-middle rounded-full"
+                    src= {post.author.photo.url}
+                    />
+                    <p className="inline align-middle text-gray-700 ml-2 text-lg">{post.author.name}</p>
+                </div>
+                <div className="font-medium text-gray-700">
+                    <svg className=" "/>
+
+                </div>
 
             </div>
 
