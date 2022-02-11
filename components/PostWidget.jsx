5d4ae6935cb00790390509b5571d53d9dc1f686b
Wafa Mohessin <wafamohessin@gmail.com>
2022-02-11 08:35:33 -0500

    index services and PostWidget.jsx passing articles

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import moment from 'moment'
+import Link from 'next/link';
 
-const PostWidget = () => {
+import { getRecentPosts } from '../services';
+
+const PostWidget = ({categories, slug}) => {
+    useState [realtedPosts, setRealatedPosts] = useState ([]);
+
+    useEffect (() =>{
+        if (slug) {
+            getSimilarPost (categories, slug)
+            .then ((result) => setRealatedPosts = (result))
+        }else{
+            getRecentPost () //we do not pass any parameter 
+            .then ((result) => setRealatedPosts = (result))
+
+        } // these Posts come from Services/index.js
+        
+
+    }, [input])
+
+
     return (
         <div>
             PostWidget
--- services/index.js
+++ services/index.js
@@ -37,3 +37,36 @@ export const getPosts =  async () => {
         const result = await request(graphqlAPI, query)
         return result.postsConnection.edges;
     }
+ export const  getRecentPosts = async ()=> {
+     const query = gql `
+     query GetPostDetails (){
+         posts(
+            orderBy:createdAt_ASC
+            last: 3
+            )
+            {
+            title
+            featuredImage {
+            url
+            }
+            createdAt
+            slug
+        }
+     }
+     `
+     const result = await request(graphqlAPI, query)
+        return result.posts;
+
+ }
+
+ export const getSimilarPost = async () => {
+     const query = gql ` 
+     query GetPostDetails($slug:String!
+        $categories:[String!]){
+            posts(
+                where:{ slug_not: $slug, AND :{categories_some: {slug_in:$categories}}} 
+                last:3
+            ) 
+        }
+     ` // do not show the current article but disply other articles that include some of the categories that you want to get. then we want to get last 3 articles.
+ }
