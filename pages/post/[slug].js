e912a102f21683331dbb10093253f6be0f7c610e
Wafa Mohessin <wafamohessin@gmail.com>
2022-02-11 20:02:27 -0500

    [slug].js

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,29 +1,47 @@
 import React from 'react'
-import { getPreEmitDiagnostics, getPostDetails } from '../../services';
+import { getPosts, getPostDetails } from '../../services';
 
-import { PostDetail, Categories, PostWidget, author, Comment, CommentForm, PostCard } from '../../components';
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from '../../components';
 
 
-
-export const Postdetails = () => {
+ const Postdetails = () => {
+     console.log(post);
     return (
         <div className="container mx-auto px-10 mb-8">
          <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
           <div className="col-span-1 lg:col-span-8">
-              <PostDetail />
-              <Author />
-              <CommentsForm />
-              <Comment />
+              <PostDetail post={post} />
+              <Author author={post.author} />
+              <CommentsForm slug={post.slug} />
+              <Comments slug={post.slug} />
 
           </div>
           <div className="col-span-1 lg:col-span-4">
            <div className="relative lg:sticky top-8">
-               <PostWidget />
+               <PostWidget 
+               slug={post.slug}
+               categories= {post.categories.map((category) => category.slug)} />
                <Categories />
            </div>
           </div>
-          
+
          </div>
         </div>
     )
+}
+
+export async function getStaticProps ({params}) {
+    const data = await getPostDetails (params,slug);
+
+    return {
+        poro= {post:data}
+    }
+}
+
+export async function getStaticPaths () {
+    const posts = await getPosts();
+    return {
+        paths: posts(({node: {slug}}) => ({params: {slug}})),
+        fallback:false,
+    } // what type of article we are going to have
 }
--- services/index.js
+++ services/index.js
@@ -93,7 +93,28 @@ export const getPosts =  async () => {
      }
      
      ` // no : becuase we are working with gql not JSON 
-     const result = await request(graphqlAPI, query)
-        return result.categories; 
+     const result = await request(graphqlAPI, query, { slug });
 
- }
+  return result.post;
+};
+
+export const getSimilarPosts = async (categories, slug) => {
+    const query = gql`
+      query GetPostDetails($slug: String!, $categories: [String!]) {
+        posts(
+          where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+          last: 3
+        ) {
+          title
+          featuredImage {
+            url
+          }
+          createdAt
+          slug
+        }
+      }
+    `;
+    const result = await request(graphqlAPI, query, { slug, categories });
+  
+    return result.posts;
+  };
